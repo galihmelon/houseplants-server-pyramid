a0ae0468786c716c2a7bd81e32e6724472466155
Dianing Yudono <dianing.yudono@shopify.com>
2021-11-09 20:10:37 +0000

    Add tests for resolvers

---
+++ houseplants/houseplants_api/test_resolvers.py
@@ -0,0 +1,57 @@
+from datetime import date, timedelta
+import pytest
+
+from .resolvers import (
+    resolve_all_plants,
+    resolve_plants_to_care,
+    resolve_water_plant,
+)
+from .test_factories import PlantFactory, WateringLogFactory
+
+
+pytestmark = pytest.mark.django_db
+
+def test_resolve_all_plants():
+    plant_1 = PlantFactory()
+    plant_2 = PlantFactory()
+    result = resolve_all_plants()
+
+    assert len(result) == 2
+    plant_ids = result.values_list('id', flat=True)
+    assert set(plant_ids) == set([plant_1.id, plant_2.id])
+
+
+def test_resolve_plants_to_care_with_no_plants():
+    result = resolve_plants_to_care()
+
+    assert len(result) == 0
+
+
+def test_resolve_plants_to_care_with_plants_and_no_logs():
+    plant = PlantFactory()
+
+    result = resolve_plants_to_care()
+
+    assert len(result) == 1
+    assert plant.id in result.values_list('id', flat=True)
+
+
+def test_resolve_plants_to_care_with_results():
+    log_a = WateringLogFactory(next_suggested_date=date.today())
+    log_b = WateringLogFactory(next_suggested_date=date.today() - timedelta(days=3))
+    log_c = WateringLogFactory(next_suggested_date=date.today() + timedelta(days=3))
+
+    result = resolve_plants_to_care()
+
+    assert len(result) == 2
+    plant_ids = result.values_list('id', flat=True)
+    assert set(plant_ids) == set([log_a.plant.id, log_b.plant.id])
+
+
+def test_resolve_water_plant():
+    PlantFactory(id=123)
+
+    result = resolve_water_plant(plant_id=123)
+
+    assert result.plant.id == 123
+    assert result.water_date == date.today()
--- houseplants/houseplants_api/tests.py
+++ houseplants/houseplants_api/tests.py
@@ -51,9 +51,9 @@ def test_plants_to_care_query_with_plants_and_no_logs():
 
 
 def test_plants_to_care_query_with_results():
-    plan_a = WateringLogFactory(next_suggested_date=date.today())
-    plan_b = WateringLogFactory(next_suggested_date=date.today() - timedelta(days=3))
-    plan_c = WateringLogFactory(next_suggested_date=date.today() + timedelta(days=3))
+    log_a = WateringLogFactory(next_suggested_date=date.today())
+    log_b = WateringLogFactory(next_suggested_date=date.today() - timedelta(days=3))
+    log_c = WateringLogFactory(next_suggested_date=date.today() + timedelta(days=3))
 
     client = Client(schema)
     executed = client.execute(
@@ -72,17 +72,17 @@ def test_plants_to_care_query_with_results():
     assert len(plants_to_care) == 2
 
     plant_ids = [int(plant['id']) for plant in plants_to_care]
-    assert plant_ids == [plan_a.plant.id, plan_b.plant.id]
+    assert plant_ids == [log_a.plant.id, log_b.plant.id]
 
 
 def test_water_plant_mutation():
-    PlantFactory(id=1)
+    PlantFactory(id=123)
 
     client = Client(schema)
     executed = client.execute(
         '''
         mutation {
-            waterPlant(plantId: 1) {
+            waterPlant(plantId: 123) {
                 wateringLog {
                     plant {
                         id
@@ -95,5 +95,5 @@ def test_water_plant_mutation():
         '''
     )
     log = executed['data']['waterPlant']['wateringLog']
-    assert log['plant']['id'] == '1'
+    assert log['plant']['id'] == '123'
     assert log['waterDate'] == date.today().strftime('%Y-%m-%d')
