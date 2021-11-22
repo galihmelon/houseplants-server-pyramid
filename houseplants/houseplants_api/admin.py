8ba9e236f5dfa1ba0eaf9e43e68839b25b37005c
Dianing Yudono <dianing.yudono@shopify.com>
2021-11-22 19:13:29 +0000

    Show CleaningLog in admin

--- houseplants/houseplants_api/admin.py
+++ houseplants/houseplants_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from houseplants_api.models import Plant, WateringLog
+from houseplants_api.models import Plant, CleaningLog, WateringLog
 
 admin.site.register(Plant)
 admin.site.register(WateringLog)
+admin.site.register(CleaningLog)
